0e8396a0c6f7b43accce9f1b254a8430d6190002
higain <maxi.stauss@t-online.de>
2017-12-07 13:41:21 +0100

    Für OSx: Andere Variante gewählt

--- src/Main.java
+++ src/Main.java
@@ -1,41 +1,45 @@
-import java.util.Map.Entry;
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
+import org.jnativehook.keyboard.NativeKeyEvent;
+import org.jnativehook.keyboard.NativeKeyListener;
 
-import lc.kra.system.keyboard.GlobalKeyboardHook;
-import lc.kra.system.keyboard.event.GlobalKeyAdapter;
-import lc.kra.system.keyboard.event.GlobalKeyEvent;
-
-public class Main {
-    private static boolean run = true;
+public class Main implements NativeKeyListener {
     private static FileLogger log;
+
+    public void nativeKeyPressed(NativeKeyEvent e) {
+        System.out.println("Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+        log.appendToLog(System.currentTimeMillis() + " " + "Key Pressed: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+
+        if (e.getKeyCode() == NativeKeyEvent.VC_ESCAPE) {
+            try {
+                GlobalScreen.unregisterNativeHook();
+            } catch (NativeHookException e1) {
+                e1.printStackTrace();
+            }
+        }
+    }
+
+    public void nativeKeyReleased(NativeKeyEvent e) {
+        System.out.println("Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+        log.appendToLog(System.currentTimeMillis() + " " + "Key Released: " + NativeKeyEvent.getKeyText(e.getKeyCode()));
+    }
+
+    public void nativeKeyTyped(NativeKeyEvent e) {
+        System.out.println("Key Typed: " + e.getKeyText(e.getKeyCode()));
+        log.appendToLog(System.currentTimeMillis() + " " + "Key Typed: " + e.getKeyText(e.getKeyCode()));
+    }
 
     public static void main(String[] args) {
         log = new FileLogger();
-        GlobalKeyboardHook keyboardHook = new GlobalKeyboardHook(false); // use false here to switch to hook instead of raw input
-        System.out.println("Global keyboard hook successfully started, press [escape] key to shutdown.");
-        /*for (Entry<Long, String> keyboard : GlobalKeyboardHook.listKeyboards().entrySet())
-            System.out.format("%d: %s\n", keyboard.getKey(), keyboard.getValue());*/
-
-        keyboardHook.addKeyListener(new GlobalKeyAdapter() {
-            @Override
-            public void keyPressed(GlobalKeyEvent event) {
-                System.out.println(event);
-                log.appendToLog(System.currentTimeMillis() + " " + Character.toString(event.getKeyChar()) + " = " + event.toString());
-
-                if (event.getVirtualKeyCode() == GlobalKeyEvent.VK_ESCAPE)
-                    run = false;
-            }
-
-            @Override
-            public void keyReleased(GlobalKeyEvent event) {
-                System.out.println(event);
-                log.appendToLog(System.currentTimeMillis() + " " + Character.toString(event.getKeyChar()) + " = " + event.toString());
-            }
-        });
-
         try {
-            while (run) Thread.sleep(128);
-        } catch (InterruptedException e) { /* nothing to do here */ } finally {
-            keyboardHook.shutdownHook();
+            GlobalScreen.registerNativeHook();
+        } catch (NativeHookException ex) {
+            System.err.println("There was a problem registering the native hook.");
+            System.err.println(ex.getMessage());
+
+            System.exit(1);
         }
+
+        GlobalScreen.addNativeKeyListener(new Main());
     }
 }
